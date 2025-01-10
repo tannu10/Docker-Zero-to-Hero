2997a295ce414cd876c3650dbea16bf9488df893
tannu10 <168409891+tannu10@users.noreply.github.com>
2025-01-10 22:02:30 +0530

    Update views.py

--- examples/python-web-app/devops/demo/views.py
+++ examples/python-web-app/devops/demo/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'demo_site.html')
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Welcome to the home page!")
+
+    
